3c5a59c6004f42b4c67b11b695d556fd6810e061
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-01 18:33:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express=require("express");
 
 const app=express(); //creating server
 app.get('/',function(req,res){
-    // res.send("Hello World");
-    res.json({Name:"Dinesh",Age:"23"}) //using express res.send--(it automatically ends the response) or res.json we can only either send json or html or normal data
-                                       //we can write res.write multiple times and end it by res.end
+    res.write("<h1>This is our first server</h1>");
+    res.write("Hello this is Node.js")
+    res.write(JSON.stringify({Name:"DineshBabu",Batch:"EA9",Course:"Full Stack Developer"}));
+    res.end();
 })
 app.listen(7000);
