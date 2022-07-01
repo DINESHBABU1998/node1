0034389c8710fe663775df86f3e649dd734d1fd4
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-01 19:23:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const data={
+    json:  {name:"Dinesh",age:"25",email:"dineshbabut25@gmail.com"},
+    json1: { name: "A", email: "AS@prepbytes.in" },
+    json2: { name: "B", email: "BS@prepbytes.in" },
+    students:[
+        {name:"dinesh",age:"18"},
+        {name:"babu",age:"20"}
+    ],
+    teachers:[
+        {name:"sasi",age:"28"},
+        {name:"kumar",age:"30"}
+    ]
+}
+module.exports=data;  //exporting the module  // or exports.dinesh = data; in app.js jsondata.dinesh
+
+
+
