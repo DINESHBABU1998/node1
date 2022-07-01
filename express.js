1ae96a46a446e848abe7d8ff7007d3deef9528ec
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-01 19:22:41 +0530

    Update express.js

--- express.js
+++ express.js
@@ -2,9 +2,12 @@ const express=require("express");
 
 const app=express(); //creating server
 app.get('/',function(req,res){
-    res.write("<h1>This is our first server</h1>");
-    res.write("Hello this is Node.js")
-    res.write(JSON.stringify({Name:"DineshBabu",Batch:"EA9",Course:"Full Stack Developer"}));
-    res.end();
+    res.json(jsondata.json);
 })
+app.get("/getstudents", function (req, res) {
+    res.json(jsondata.students);
+});
+app.get("/getteachers", function (req, res) {
+    res.json(jsondata.teachers);
+});
 app.listen(7000);
