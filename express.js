98a5c566d76d20a52885443a18962b054a1f20e3
T DINESH BABU <98023477+DINESHBABU1998@users.noreply.github.com>
2022-07-01 19:29:36 +0530

    Update express.js

--- express.js
+++ express.js
@@ -10,4 +10,7 @@ app.get("/getstudents", function (req, res) {
 app.get("/getteachers", function (req, res) {
     res.json(jsondata.teachers);
 });
+app.get("/express", function (req, res) {
+    res.send("<h1>Express is a minimal and flexible Node.js web application framework that provides a robust set of features to develop web and mobile applications. It facilitates the rapid development of Node based Web applications.</h1>")
+});
 app.listen(7000);
